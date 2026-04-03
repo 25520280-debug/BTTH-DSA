ce2f84d56930f61f35574d5a86f2ac78c66dfade
Minh Đạt <25520280@gm.uit.edu.vn>
2026-04-03 21:14:10 +0700

    Update BT3.

--- Week-2/BT3-(Heap_Sort).cpp
+++ Week-2/BT3-(Heap_Sort).cpp
@@ -21,18 +21,33 @@ void heapSort(vector <int> &v)
     int n = v.size();
     for (int i = n/2 - 1; i >= 0; i--)
         heapify(v,n,i);
+    cout << "Mang sau khi BuildHeap: ";
+    for (int x : v) cout << x << ' ';
 
     for (int i = n-1; i >= 0; i--)
     {
+        cout << "\nSwap " << v[0] << " va " << v[i] << "\n\n";
         swap(v[0],v[i]);
         heapify(v,i,0);
+        cout << "Mang sau khi heapify voi n = " << i << ": ";
+        for (int j = 0; j < v.size(); j++)
+        {
+            cout << v[j] << ' ';
+            if (j == i-1) cout << "| ";
+        }
     }
 }
 
 int main()
 {
-    vector <int> v {1,5,6,2,3,4,8,7,9};
+    int n; cin >> n;
+    vector <int> v(n);
+    for (int &x : v) cin >> x;
+    cout << "Mang truoc khi sap xep:\n";
+    for (int x : v) cout << x << ' ';
+    cout << "\n\nSap xep:\n";
     heapSort(v);
+    cout << "\nMang sau khi sap xep:\n";
     for (int x : v) cout << x << ' ';
     return 0;
 }
